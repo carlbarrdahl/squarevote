6eb29274a5a0beb549ea29bdc9740f7a2a1af5c9
Carl Barrdahl <carlbarrdahl@gmail.com>
2024-02-20 15:54:00 +0100

    Update middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,8 +4,7 @@ import { authMiddleware } from "@clerk/nextjs";
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
 export default authMiddleware({
-  publicRoutes: ["/", "/sign-in", "/opengraph-image", "/poll(.*)"],
-  ignoredRoutes: ["/opengraph-image"],
+  publicRoutes: (req) => !req.url.includes("/dashboard"),
 });
 
 export const config = {
